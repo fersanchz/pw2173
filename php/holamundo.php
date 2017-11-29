6584fdc66f80de28fa72213cdd004e3df5402de2
fersanchz <safernando50@gmail.com>
2017-11-29 15:57:44 -0700

    inicio de php

---
+++ php/holamundo.php
@@ -0,0 +1,10 @@
+<?php
+	echo "Hola Mundo por echo(forma incorrecta porque manda a consola y a pantalla)";
+	print "Hola mundo por print (es la forma correcta, manda solo a la pantalla)";
+
+
+	$aquihayunavariable=0; //todas las variables empiezan con $
+	for($i=0; $i<10; $i++){
+		print "Hola mundo<br>";
+	}
+?>
